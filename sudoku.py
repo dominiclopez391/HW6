01e4f7f14335312f71486568c375c04f989ee29d
dominiclopez391 <dominic.lopez@sjsu.edu>
2023-03-05 19:15:05 -0800

    working on build_csp

--- sudoku.py
+++ sudoku.py
@@ -24,8 +24,53 @@ def build_csp(puzzle):
     (row, column) representing the filled puzzle squares and the values
     are the corresponding numbers assigned to these squares.
     :return: Csp object
+
+    domains: a dictionary representing variables and their domains.
+        The dictionary keys are variable names and the values are sets
+        representing their domains.
+    neighbors: a dictionary representing binary constraints.
+        The dictionary keys are variable names and the values are sets
+        containing all the variables that are connected to the key.
+        (Variables are connected if they both appear in a constraint)
+    constraint: a function that takes as arguments two variables
+        and two values: f(var1, val1, var2, val2).
+        The function returns True if var1 and var2 satisfy the
+        constraint when their respective values are val1 and val2.
     """
-    # Enter your code here and remove the pass statement below
+
+    dictForCSP = {}
+    for i in range(9):
+        for j in range(9):
+            if not (i, j) in puzzle:
+                dictForCSP[(i, j)] = range(9)
+            else:
+                dictForCSP[(i, j)] = puzzle[(i, j)]
+
+    neighborsForCSP = {}
+    for i in range(9):
+        for j in range(9):
+            neighborsForCSP[(i, j)] = set()
+
+            # add each element as a constraint
+
+            #first 3x3 grid elements
+            for k in range(3):
+                for l in range(3):
+
+                    #get every element in the corresponding 3x3 sudoku grid area for each variable
+                    connectedVar = (((i - 1) // 3) * 3 + k, ((j - 1) // 3) * 3 + l);
+                    if connectedVar != (i, j):
+                        neighborsForCSP[(i, j)].add(connectedVar)
+
+            #next row elements
+            for k in range(9):
+                if not (i, j) == (k, j) and not (k, j) in neighborsForCSP[(i, j)]:
+                    neighborsForCSP[(i, j)].add((k, j))
+
+            #next column elements
+            for k in range(9):
+                if not (i, j) == (i, k) and not (i, k) in neighborsForCSP[(i, j)]:
+                    neighborsForCSP[(i, j)].add((i, k))
     pass
 
 
