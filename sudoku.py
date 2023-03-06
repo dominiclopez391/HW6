24918e5797074f672601dc0491ff1e89d38e3eda
dominiclopez391 <dominic.lopez@sjsu.edu>
2023-03-05 18:25:52 -0800

    added project files

---
+++ sudoku.py
@@ -0,0 +1,70 @@
+# ----------------------------------------------------------------------
+# Name:     sudoku
+# Purpose:  Homework6
+#
+# Author(s):
+#
+# ----------------------------------------------------------------------
+"""
+Sudoku puzzle solver implementation
+
+q1:  Basic Backtracking Search
+q2:  Backtracking Search with AC-3
+q3:  Backtracking Search with MRV Ordering and AC-3
+"""
+import csp
+
+# Enter your helper functions here
+
+
+def build_csp(puzzle):
+    """
+    Create a Csp object representing the puzzle.
+    :param puzzle: (dictionary) the dictionary keys are tuples
+    (row, column) representing the filled puzzle squares and the values
+    are the corresponding numbers assigned to these squares.
+    :return: Csp object
+    """
+    # Enter your code here and remove the pass statement below
+    pass
+
+
+def q1(puzzle):
+    """
+    Solve the given puzzle with basic backtracking search
+    :param puzzle: (dictionary) the dictionary keys are tuples
+    (row, column) representing the filled puzzle squares and the values
+    are the corresponding numbers assigned to these squares.
+    :return: a tuple consisting of a solution (dictionary) and the
+    Csp object.
+    """
+    # Enter your code here and remove the pass statement below
+    pass
+
+
+def q2(puzzle):
+    """
+    Solve the given puzzle with backtracking search and AC-3 as
+    a preprocessing step.
+    :param puzzle: (dictionary) the dictionary keys are tuples
+    (row, column) representing the filled puzzle squares and the values
+    are the corresponding numbers assigned to these squares.
+    :return: a tuple consisting of a solution (dictionary) and the
+    Csp object.
+    """
+    # Enter your code here and remove the pass statement below
+    pass
+
+
+def q3(puzzle):
+    """
+    Solve the given puzzle with backtracking search and MRV ordering and
+    AC-3 as a preprocessing step.
+    :param puzzle: (dictionary) the dictionary keys are tuples
+    (row, column) representing the filled puzzle squares and the values
+    are the corresponding numbers assigned to these squares.
+    :return: a tuple consisting of a solution (dictionary) and the
+    Csp object.
+    """
+    # Enter your code here and remove the pass statement below
+    pass
